bb54f9374b85a6b950c36c8bb96aa1a2f503fd0c
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-03-16 21:05:53 +0300

    assignment completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+puts 'seeding...'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: 'Luke', last_name: 'Azular', grade: 40)
+Student.create(first_name: 'Jim', last_name: 'Kazach', grade: 90)
+Student.create(first_name: 'Alita', last_name: 'Catelo', grade: 60)
+
+puts 'seeding done.'
